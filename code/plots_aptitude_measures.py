bfbb22ab305e91bee8d69576758e131358ff3a7c
carodelgorm <78938551+carodelgorm@users.noreply.github.com>
2021-07-15 18:29:14 -0400

    added plots

--- code/plots_aptitude_measures.py
+++ code/plots_aptitude_measures.py
@@ -15,36 +15,32 @@ from setup_plots import get_dataset
 os.getcwd()
 
 # %%
-# change working directory so all files save in the same spot
+# change working directory to a separate folder for plots
 os.chdir('C:/Users/bec10/OneDrive/Desktop/files/repos/gorman-earlyjobskills-analysis/out/heatmaps')
 
 # %%
 # Pull in the data
 df = get_dataset()
-df 
+df.columns 
 
 # %%
-df.columns
+df['GENDER'].unique()
 
 # %%
-df['AFQT_1'].describe()
-
-# %%
-df['ROTTER_SCORE'].describe()
-
-# %%
-df['ROSENBERG_SCORE'].describe()
+df['RACE'].unique()
 
 # %%
 # Check the values of the variable age 
+# The oldest age that all respondents have data for in the dataset is 47
+# The youngest age that all respondents have data for in the dataset is 22
 df['AGE'].value_counts().sort_values(ascending=True)
 
 # %%
-# set up 
-cond = df['AGE'].isin([18])
+# Examine the basic relationship between aptitude / attitude scores and hourly wages at 23
+cond = df['AGE'].isin([23])
 df = df[cond]
 
-# %%
+# Plot the relationships in heatmaps 
 for parent in ['AFQT', 'ROSENBERG', 'ROTTER']:
     ax = plt.figure().add_subplot(111)
 
@@ -62,18 +58,28 @@ for parent in ['AFQT', 'ROSENBERG', 'ROTTER']:
     tab = round(tab,2)
     hm = sns.heatmap(tab, cmap="Blues", vmin=0, vmax=0.15, annot=True)
     hm.invert_yaxis()
+
+    csfont = {'fontname':'Times New Roman'}
     ax.set_yticks(np.linspace(0.5, 3.5, 4))
     ax.set_yticklabels(range(1, 5))
     ax.set_ylabel('Hourly Wages (quartiles)')
 
+   
+
     ax.set_xticks(np.linspace(0.5, 3.5, 4))
     ax.set_xticklabels(range(1, 5))
-    ax.set_xlabel(ylabel + ' Scores (quartiles)')
-
-    #plt.savefig('fig-human-capital-' + parent.lower() + '.png')
-
+    ax.set_xlabel(ylabel + ' Scores (quartiles)', **csfont)
+    
+    plt.savefig('fig-apt-att-23' + parent.lower() + '.png')
 
 # %%
+# Examine the relationship for later age
+df = get_dataset()
+
+cond = df['AGE'].isin([47])
+df = df[cond]
+
+# Plot the relationships in heatmaps 
 for parent in ['AFQT', 'ROSENBERG', 'ROTTER']:
     ax = plt.figure().add_subplot(111)
 
@@ -85,19 +91,23 @@ for parent in ['AFQT', 'ROSENBERG', 'ROTTER']:
         ylabel = parent.lower().capitalize()
 
     x = pd.qcut(df[label], 4, labels=False, duplicates="drop")
-    y = pd.qcut(df['INCOME_WAGES_SALARY'], 4, labels=False, duplicates="drop")
+    y = pd.qcut(df['WAGE_HOURLY_JOB_1'], 4, labels=False, duplicates="drop")
 
     tab = pd.crosstab(y, x, normalize=True)
     tab = round(tab,2)
     hm = sns.heatmap(tab, cmap="Blues", vmin=0, vmax=0.15, annot=True)
     hm.invert_yaxis()
+
+    csfont = {'fontname':'Times New Roman'}
     ax.set_yticks(np.linspace(0.5, 3.5, 4))
     ax.set_yticklabels(range(1, 5))
     ax.set_ylabel('Hourly Wages (quartiles)')
 
+
     ax.set_xticks(np.linspace(0.5, 3.5, 4))
     ax.set_xticklabels(range(1, 5))
-    ax.set_xlabel(ylabel + ' Scores (quartiles)')
+    ax.set_xlabel(ylabel + ' Scores (quartiles)', **csfont)
+    
+    plt.savefig('fig-apt-att-47' + parent.lower() + '.png')
 
-    # plt.savefig('fig-human-capital-' + parent.lower() + '.png')
-# %%
+
